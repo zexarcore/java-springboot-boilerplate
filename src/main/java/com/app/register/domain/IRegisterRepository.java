69eba0b64b116ff98548866e68046336263a436c
Michael Steven Galvis Buritica <buriticamaicol490@gmail.com>
2025-05-21 20:47:38 -0500

    (fix(domain of register)

--- src/main/java/com/app/register/domain/IRegisterRepository.java
+++ src/main/java/com/app/register/domain/IRegisterRepository.java
@@ -10,34 +10,25 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IRegisterRepository extends JpaRepository<Register, Long> {
 
-
     @Override
     @NonNull
     List<Register> findAll();
 
-   
     @Override
     @NonNull
     Optional<Register> findById(@NonNull Long id);
 
-   
     @Override
     @NonNull
     <S extends Register> S save(@NonNull S register);
 
-   
     @Override
     boolean existsById(@NonNull Long id);
 
-    
     @Override
     void deleteById(@NonNull Long id);
 
-  
-
- 
     Optional<Register> findByEmail(String email);
 
-   
     boolean existsByEmail(String email);
 }
--- src/main/java/com/app/register/domain/IRegisterService.java
+++ src/main/java/com/app/register/domain/IRegisterService.java
@@ -4,18 +4,13 @@ import java.util.List;
 
 public interface IRegisterService {
 
-  
     List<Register> findAll();
 
-    
     Register findById(Long id);
 
-    
     Register save(Register register);
 
-    
     Register update(Register register, Long id);
 
-   
     void deleteById(Long id);
 }
